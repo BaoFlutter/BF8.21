07fece714f562ce069cdd9bbd216ae2ad678e83f
BaoFlutter <baoflutter@gmail.com>
2021-09-10 22:04:08 +0700

    add state

---
+++ state_flutter/lib/views/information_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:widget_flutter/resources/widgets/common_widget.dart';
+class InformationScreen extends StatelessWidget {
+  final String userName;
+  final String age;
+  const InformationScreen({Key? key, required this.userName, required this.age}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Information"),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: informationWidget(
+              userNameContent: userName,
+              ageContent: age),
+        ),
+
+      ),
+    );
+  }
+
+
+}
---
+++ state_flutter/lib/views/state_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:widget_flutter/resources/widgets/common_widget.dart';
+class StateWidget extends StatefulWidget {
+  final String userName ;
+  final String age;
+
+  const StateWidget({Key? key, required this.userName,required this.age}) : super(key: key);
+  @override
+  _StateWidgetState createState() => _StateWidgetState();
+}
+
+class _StateWidgetState extends State<StateWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Information"),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios_outlined),
+          onPressed: (){
+            //Get.back();
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: informationWidget(
+              userNameContent: widget.userName,
+              ageContent: widget.age),
+        ),
+
+      ),
+    );
+  }
+
+
+}
