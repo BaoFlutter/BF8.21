66089c2a8fece927f2f99d42d3406d41d597589c
BaoFlutter <baoflutter@gmail.com>
2021-10-11 22:00:27 +0700

    add conversation

---
+++ achat_2/lib/views/main_screen.dart
@@ -0,0 +1,23 @@
+import 'package:achat/controllers/firebase_auth_controller.dart';
+import 'package:achat/views/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'auth/sign_in_screen.dart';
+import 'home_screen.dart';
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final status = context.watch<FirebaseAuthController>().authStatus;
+    switch(status){
+      case AuthStatus.authenticate:
+        return HomeScreen();
+      case AuthStatus.unauthenticate:
+        return SignInScreen();
+      default:
+        return SplashScreen();
+    }
+  }
+}
